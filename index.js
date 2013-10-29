f3d99710faa0e295cf30b2f86ecb18a3558004ea
Jérémy Lal <kapouer@melix.org>
2013-10-29 21:39:25 +0100

    http.METHODS transition by the hand
    `require('methods');` and http.METHODS
    is set. Backward and forward compatible.

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
-
 var http = require('http');
 
 if (http.METHODS) {
   module.exports = http.METHODS.map(function(method){
     return method.toLowerCase();
   });
-
-  return;
+} else {
+  http.METHODS = module.exports = [
+    'get',
+    'post',
+    'put',
+    'head',
+    'delete',
+    'options',
+    'trace',
+    'copy',
+    'lock',
+    'mkcol',
+    'move',
+    'propfind',
+    'proppatch',
+    'unlock',
+    'report',
+    'mkactivity',
+    'checkout',
+    'merge',
+    'm-search',
+    'notify',
+    'subscribe',
+    'unsubscribe',
+    'patch',
+    'search'
+  ];
 }
-
-module.exports = [
-  'get',
-  'post',
-  'put',
-  'head',
-  'delete',
-  'options',
-  'trace',
-  'copy',
-  'lock',
-  'mkcol',
-  'move',
-  'propfind',
-  'proppatch',
-  'unlock',
-  'report',
-  'mkactivity',
-  'checkout',
-  'merge',
-  'm-search',
-  'notify',
-  'subscribe',
-  'unsubscribe',
-  'patch',
-  'search'
-];
